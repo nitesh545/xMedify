54bd06a82d010b8992c49e3f2911ec778e05f4a3
nitecse <hitman12457868@gmail.com>
2024-10-23 01:13:28 +0530

    Component: Home

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PatientCaring from "./Components/PatientCaring";
 import OurFamilies from "./Components/OurFamilies";
 import Footer from "./Components/Footer";
 import DownlaodApp from "./Components/DownlaodApp";
+import Home from "./Components/Home";
 
 const theme = createTheme();
 
@@ -22,6 +23,7 @@ function App() {
 		<ThemeProvider theme={theme}>
 			<CssBaseline/>
 			<Navbar/>
+			<Home />
 			<OurFamilies/>
 			<PatientCaring/>
 			<FrequentlyAskedQuestions/>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,15 +5,15 @@ import logo from '../assets/logo.png'
 export default function Navbar() {
 	let btns = ["Find Doctors", "Hospitals", "Medicines", "Surgeries", "Software for Provider", "Facilities"];
 	return (
-		<AppBar sx={{position: 'static'}} elevation={0}>
-			<Toolbar>
+		<AppBar sx={{position: 'fixed', backgroundColor: 'transparent', boxShadow: 'none'}} elevation={0}>
+			<Toolbar sx={{backgroundColor: 'rgba(42, 167, 255, 1)'}}>
 				<Typography variant="body1" align='center' sx={{flexGrow: 1}}>The health and well-being of our patients
 					and their health care team will always be our priority, so we follow the best practices for
 					cleanliness.</Typography>
 			</Toolbar>
-			<Toolbar sx={{backgroundColor: 'white'}}>
-				<Box sx={{display: 'flex', justifyContent: 'space-between', width: '100vw', margin: '2rem'}}>
-					<Box component="img" src={logo} alt="App Image" sx={{height: '1.75rem',}}/>
+			<Toolbar>
+				<Box sx={{display: 'flex', justifyContent: 'space-between', width: '100vw', margin: '0.5rem'}}>
+					<Box component="img" src={logo} alt="App Image" sx={{height: '2rem', marginLeft: '8rem', my: '0.5rem'}}/>
 					<Box sx={{}}>
 						{
 							btns.map((btn, index) => (
